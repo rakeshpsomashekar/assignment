5b37f8abca753bccf8fcc98478f3f6afd131914e
rakeshpsomashekar <raps@deloitte.com>
2024-08-09 08:09:43 +0530

    Update app.py
    added tags for all features

--- main/app.py
+++ main/app.py
@@ -7,14 +7,11 @@ app=FastAPI()
 
 Base.metadata.create_all(engine)
 
-app.include_router(auth.router,prefix='/auth')
+app.include_router(auth.router,prefix='/auth',tags=['AUTHENTICATION APIs'])
+app.include_router(admin.router,prefix='/admin',tags=['ADMIN APIs'])
+app.include_router(customer.router,prefix='/customer',tags=['CUSTOMER APIs'])
+app.include_router(delivery.router,prefix='/delivery',tags=['DELIVERY_PARTNER APIs'])
 
-
-
-
-app.include_router(admin.router,prefix='/admin')
-app.include_router(customer.router,prefix='/customer')
-app.include_router(delivery.router,prefix='/delivery')
 
 @app.get('/')
 def read_root():
